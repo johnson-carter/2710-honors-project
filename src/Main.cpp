60c9e627bb68bc317b5e29f4024f6836c03bb288
Carter J <carter.johnson44@outlook.com>
2026-04-02 11:16:21 -0500

    moved quiz editing menu to quizSetup function. Simplifying main function

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 // Function declarations:
 void showGreeting();
+void quizSetup();
 void generateQuestion();
 void editQuestion();
 void deleteQuestion();
@@ -31,6 +32,22 @@ int numQuestions = 0;
 int main() {
     showGreeting();
 
+    quizSetup();
+    
+    TestSession session(firstQuestion);
+    session.startQuiz();
+
+    cout << "\n*** Thank you for using the testing service. Goodbye! ***\n";
+
+    return 0;
+}
+
+
+void showGreeting(){
+    cout << "*** Welcome to the Honors' Testing Service ***\n\n";
+}
+
+void quizSetup(){
     while(true){
         cout << "Do you want to?\n\t1. Create new question.\n\t2. Edit question.\n\t3. Delete question.\n\t4. Finish\n";
         cout << "Select an action: ";
@@ -63,19 +80,7 @@ int main() {
         if(menuOption == 4) break;
     }
 
-    TestSession session(firstQuestion);
-    session.startQuiz();
-
-    cout << "\n*** Thank you for using the testing service. Goodbye! ***\n";
-
-    return 0;
 }
-
-
-void showGreeting(){
-    cout << "*** Welcome to the Honors' Testing Service ***\n\n";
-}
-
 void generateQuestion(){
     ++numQuestions;
 
