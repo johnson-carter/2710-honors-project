7987338b6130929e3e514f167aa8eba44547ecfa
Carter J <carter.johnson44@outlook.com>
2026-04-02 08:37:17 -0500

    Answer input formatting

--- src/TestSession.cpp
+++ src/TestSession.cpp
@@ -28,6 +28,7 @@ struct TestSession{
         char result;
         while(true){
             string resultStr;
+            cout << "Your answer: ";
             getline(cin, resultStr);
             if(resultStr.length() == 1 && isalpha(resultStr[0])){
                 result = toupper(resultStr[0]);
@@ -43,6 +44,7 @@ struct TestSession{
         bool result;
         while(true){
             string resultStr;
+            cout << "Your answer [true/false]: ";
             getline(cin, resultStr);
             if(resultStr == "true"){
                 result = true;
@@ -59,13 +61,15 @@ struct TestSession{
 
     string getWRAnswer(){
         string result;
+        // should answers be case sensitive? They are by default
+        cout << "Your answer: ";
         getline(cin, result);
         return result;
     }
 
     // Returns true if guessed correctly.
     bool askQuestion(){
-        cout << "Question " << totalQuestions << ": " << testQuestion->questionContent << "\n\n";
+        cout << "\nQuestion " << totalQuestions << ": " << testQuestion->questionContent << "\n";
         bool isCorrect = false;
 
         switch(testQuestion->questionType){
@@ -85,7 +89,6 @@ struct TestSession{
                 break;
             //////
             case LinkedQuestion::TFQ:{
-                cout << "True or False? \n";
                 bool tfAnswer = getTFAnswer();
                 
                 if(tfAnswer == testQuestion->isTrue){
@@ -121,8 +124,11 @@ struct TestSession{
             ++totalQuestions;
             totalPoints += testQuestion->pointValue;
             if(askQuestion()){
+                cout << "[Your answer is correct!]\n";
                 ++correctQuestions;
                 earnedPoints += testQuestion->pointValue;
+            } else{
+                cout << "[Your answer is incorrect!]\n";
             }
 
             testQuestion = testQuestion->nextQuestion;
