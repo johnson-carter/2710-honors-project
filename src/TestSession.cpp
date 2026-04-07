a8d5dd34ce4fedb2e687137d48df09d2138b4743
natalietvn <natalietn05@gmail.com>
2026-04-07 00:20:21 -0500

    Update TestSession.cpp

--- src/TestSession.cpp
+++ src/TestSession.cpp
@@ -10,6 +10,7 @@ struct TestSession{
     void printSessionResults();*/
 
     // Fields
+    LinkedQuestion* head;
     LinkedQuestion* testQuestion;
     double totalPoints;
     double earnedPoints;
@@ -17,11 +18,126 @@ struct TestSession{
     int correctQuestions;
 
     TestSession(LinkedQuestion* q){
+        head = q;
         testQuestion = q;
         totalPoints = 0;
         earnedPoints = 0;
         totalQuestions = 0;
         correctQuestions = 0;
+    }
+
+    // logic for Phase 2 "Jump to Question"
+    void jumpToQuestion(int targetNum) {
+        if (targetNum < 1) {
+            cout << "[That question does not exist!]\n";
+            return;
+        }
+        LinkedQuestion* temp = head;
+        for (int i = 1; i < targetNum && temp != nullptr; i++) {
+            temp = temp->nextQuestion;
+        }
+        if (temp == nullptr) {
+            cout << "[That question does not exist!]\n";
+        }
+        else {
+            testQuestion = temp;
+            processCurrentQuestion(targetNum);
+        }
+    }
+    void processCurrentQuestion(int index) {
+        if (testQuestion->isAnswered) {
+            cout << "Question " << targetNum << ": " << testQuestion->questionContent << endl;
+            cout << "You answered: " << testQuestion->studentAnswer << endl;
+                
+            cout << "\nDo you want to?\n";
+            cout << "1. Edit this answer.\n";
+            cout << "2. Go to next question\n";
+            cout << "3. Jump to question.\n";
+            cout << "4. Submit.\n";
+            cout << "Select an action: ";
+            string action;
+            getline(cin, action);
+
+            if (action == "1") {
+                askForNewAnswer();
+            }
+            else if (action == "2") {
+                moveToNextUnanswered();
+            }
+            else if (action == "3") {
+                handleJumpInput();
+            }
+        }
+        else {
+            cout << "\nQuestion " << index << ": " << testQuestion->questionText << endl;
+            askForNewAnswer();
+        }
+    }
+    void askForNewAnswer() {
+        cout << "Your answer: ";
+        getline(cin, testQuestion->studentAnswer);
+        testQuestion->isAnswered = true;
+    }
+    void moveToNextUnanswered() {
+        LinkedQuestion* search = testQuestion->nextQuestion;
+        while (search != nullptr) {
+            if (!search->isAnswered) {
+                testQuestion = search;
+                return;
+            }
+            search = search->nextQuestion;
+        }
+        search = head;
+        while (search != testQuestion && search != nullptr) {
+            if (!search->isAnswered) {
+                testQuestion = search;
+                return;
+            }
+            search = search->nextQuestion;
+        }
+        cout << "[All questions answered! Select 'Submit' to finish.]\n";
+    }
+    void handleJumpInput() {
+        cout << "Jump to question [1-4]: "; // Dynamically calculate max in production
+        string input;
+        getline(cin, input);
+        try {
+            jumpToQuestion(stoi(input));
+        } catch (...) {
+            cout << "[Invalid input!]\n";
+        }
+    }
+    void printSessionResults() {
+        cout << "\n=== SESSION LOG ===\n";
+        LinkedQuestion* curr = head;
+        int count = 1;
+
+        while (curr != nullptr) {
+            bool isCorrect = compareAnswers(curr->studentAnswer, curr->correctAnswer);
+            if (isCorrect) {
+                earnedPoints += curr->points;
+                correctQuestions++;
+            }
+            cout << "Question " << count << ": " << curr->correctAnswer << endl;
+            cout << "Your answer: " << curr->studentAnswer << endl;
+            curr = curr->nextQuestion;
+            count++;
+        }
+
+        cout << "\nCorrect answers: " << correctQuestions << "/" << totalQuestions << endl;
+        cout << "Final score: " << earnedPoints << "/" << totalPoints << endl;
+        cout << "*** Thank you for using the testing service. Goodbye! ***\n";
+    }
+    bool compareAnswers(string s1, string s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        for (size_t i = 0; i < s1.length(); i++) {
+            if (toupper(s1[i]) != toupper(s2[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     char getMCQAnswer(){
